bbe5a5cb27bf4c099f0341e32591f1a34c9e3875
sabriguantay <52327724+sabriguantay@users.noreply.github.com>
2019-10-19 04:02:46 -0300

    Update InmuebleRepository.java

--- inmobiliaria/src/main/java/ar/com/api/inmobiliaria/repo/InmuebleRepository.java
+++ inmobiliaria/src/main/java/ar/com/api/inmobiliaria/repo/InmuebleRepository.java
@@ -4,11 +4,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ar.com.api.inmobiliaria.entities.Inmueble;
 
+import java.util.*;
+
+
+
 /**
  * InmuebleRepository
  */
 public interface InmuebleRepository extends JpaRepository<Inmueble, Integer>{
 
+ List<Inmueble> findAllByTotalAmbientes(int nroAmbientes);
+
+    List<Inmueble> findAllByCantBaños(int cantBanios);
+
+    List<Inmueble> findAllByTipoInmueble(String tipo);
     
+    List<Inmueble> findAllByBarrio(String barrio);
+
+    List<Inmueble> findAllByFinalidad(String finalidad);
+
+    List<Inmueble>  findAllOrderByImporte();
+
+    List<Inmueble> findAllByLocalidad(String localidad);
+
+    List<Inmueble> findAllByNroDormitorios(int nroDorm);   
     
 }
