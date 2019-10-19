255ceed3874641956466b6a803fbde50edf058c9
sabriguantay <52327724+sabriguantay@users.noreply.github.com>
2019-10-19 04:08:15 -0300

    Update InmuebleService.java

--- inmobiliaria/src/main/java/ar/com/api/inmobiliaria/services/InmuebleService.java
+++ inmobiliaria/src/main/java/ar/com/api/inmobiliaria/services/InmuebleService.java
@@ -13,5 +13,76 @@ public class InmuebleService {
 
     @Autowired
     InmuebleRepository repo;
+    
+    public int crearInmueble(String moneda, double valor, int tipoInmueble, String direccion,int superficieTotal, int totalAmbientes, int nroDormitorios, int cantBaños, int finalidad, String detalles, String barrio,){
+        Inmueble i = new Inmueble();
+        i.setMoneda();
+        i.setValor();
+        i.setTipoInmueble();
+        i.setDireccion();
+        i.setBarrio();
+        i.setSuperficieTotal();
+        i.setTotalAmbientes();
+        i.setNroDormitorios();
+        i.setCantBanios();
+        i.setFinalidad();
+        i.setDetalles();
+        
+        repo.save(i);
+        return i.getId();
+    }
 
+  public List<Inmueble> listarInmuebles() {
+    return repo.findAll();
+
+  }
+
+  public Inmueble buscarPorId(int id) {
+
+    Optional<Inmueble> i = repo.findById(id);
+
+    if (i.isPresent()) {
+      return i.get();
+    }
+    return null;
+  }
+
+  public List<Inmueble> buscarInmueblesOrdenadosPorImporte() {
+    return repo.findAllOrderByImporte();
+  }
+
+  public List<Inmueble> buscarInmueblesPorBarrio(String barrio) {
+    return repo.findAllByBarrio(barrio);
+
+  }
+
+  public List<Inmueble> buscarInmueblesPorTipoInmueble(String tipoInm) {
+    return repo.findAllByTipoInmueble(tipoInm);
+  }
+
+  public List<Inmueble> buscarInmueblesPorTotalAmbientes(int totalAmb) {
+
+    return repo.findAllByTotalAmbientes(totalAmb);
+
+  }
+
+  public List<Inmueble> buscarPorLocalidad(String localidad) {
+
+    return repo.findAllByLocalidad(localidad);
+  }
+
+  public List<Inmueble> buscarPorFinalidad(String fin) {
+
+    return repo.findAllByFinalidad(fin);
+  }
+
+  public List<Inmueble> buscarPorDormitorios(int nroDorm) {
+
+    return repo.findAllByNroDormitorios(nroDorm);
+  }
+
+  public List<Inmueble> buscarPorCantBanios(int banios) {
+
+    return repo.findAllByCantBanios(banios);
+  }
 }
