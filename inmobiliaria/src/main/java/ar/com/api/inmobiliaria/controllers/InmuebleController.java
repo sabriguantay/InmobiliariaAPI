1cc4bd40fdfcf0d163dab0a6d7c53c8dfaaa0ad4
sabriguantay <sabrinaguantay2@hotmail.com>
2019-10-19 03:23:32 -0300

    -

--- inmobiliaria/src/main/java/ar/com/api/inmobiliaria/controllers/InmuebleController.java
+++ inmobiliaria/src/main/java/ar/com/api/inmobiliaria/controllers/InmuebleController.java
@@ -9,39 +9,6 @@ public class InmuebleController {
     @Autowired
     InmuebleService is;
 
-    @GetMapping("/inmuebles/barrios")
-    public ResponseEntity<Inmueble> getInmuebleByBarrio(
-            @RequestParam(value = "barrio", required = false) String barrio) {
-        List<Inmueble> li;
-        inmuebleService.buscarPorBarrio(barrio);
-
-        if (barrio == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return ResponseEntity.ok(li);
-    }
-
-    @GetMapping("/inmuebles/searchtipoInmueble=")
-    public ResponseEntity<Inmueble> getInmuebleByBarrio(
-            @RequestParam(value = "tipoInmueble", required = false) String barrio) {
-        List<Inmueble> li;
-        inmuebleService.buscarPorBarrio(barrio);
-
-        if (tipoInmueble == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return ResponseEntity.ok(li);
-    }
-
-    @GetMapping("/inmuebles/searchTotalAmbientes=")
-    public ResponseEntity<Inmueble> getInmuebleBy(@RequestParam(value = "barrio", required = false) String barrio) {
-        List<Inmueble> li;
-        inmuebleService.buscarPorBarrio(barrio);
-
-        if (barrio == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return ResponseEntity.ok(li);
-    }
+    
 
 }
