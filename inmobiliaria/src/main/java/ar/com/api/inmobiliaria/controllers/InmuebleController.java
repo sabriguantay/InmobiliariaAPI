2e1189c52bc3b2f508945fdad0a6fa19d17f1ca8
sabriguantay <52327724+sabriguantay@users.noreply.github.com>
2019-10-19 04:00:24 -0300

    Update InmuebleController.java

--- inmobiliaria/src/main/java/ar/com/api/inmobiliaria/controllers/InmuebleController.java
+++ inmobiliaria/src/main/java/ar/com/api/inmobiliaria/controllers/InmuebleController.java
@@ -9,6 +9,41 @@ public class InmuebleController {
     @Autowired
     InmuebleService is;
 
-    
+    @GetMapping("/inmuebles/searchbarrio=")
+    public ResponseEntity<Inmueble> getInmuebleByBarrio(
+            @RequestParam(value = "barrio", required = false) String barrio) {
+        List<Inmueble> li;
+        is.buscarInmueblesPorBarrio(barrio);
+
+        if (barrio == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(li);
+    }
+
+    @GetMapping("/inmuebles/tipoInmueble")
+    public ResponseEntity<Inmueble> getInmuebleByTipoInmueble(
+            @RequestParam(value = "tipoInmueble", required = false) String tipo) {
+        List<Inmueble> li;
+        is.buscarInmueblesPorTipoInmueble(tipo);
+
+        if (tipo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(li);
+    }
+
+    @GetMapping("/inmuebles/searchTotalAmbientes=")
+    public ResponseEntity<Inmueble> getInmuebleByTotalAmbientes(
+            @RequestParam(value = "barrio", required = false) int totalAmb) {
+        List<Inmueble> li;
+        is.buscarInmueblesPorTotalAmbientes(totalAmb);
+
+        if (totalAmb < 0) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(li);
+    }
+
 
 }
