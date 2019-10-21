a12fc0df9caf63b27582490989d802186ab61886
sabriguantay <sabriguantay@users.noreply.github.com>
2019-10-21 11:38:36 -0300

    -

--- inmobiliaria/src/main/java/ar/com/api/inmobiliaria/services/UsuarioService.java
+++ inmobiliaria/src/main/java/ar/com/api/inmobiliaria/services/UsuarioService.java
@@ -1,11 +1,8 @@
 package ar.com.api.inmobiliaria.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ar.com.api.inmobiliaria.entities.Usuario;
 import ar.com.api.inmobiliaria.repo.UsuarioRepository;
 
 /**
@@ -15,25 +12,10 @@ import ar.com.api.inmobiliaria.repo.UsuarioRepository;
 public class UsuarioService {
 
     @Autowired
-    UsuarioRepository repo;
+    UsuarioRepository usuarioRepository;
 
-     public Usuario buscarPorId(int id){
-        Optional<Usuario> u = repo.findById(id);
-        
-        if (u.isPresent())
-            return u.get();
-        return null;
 
-    }
-
-    public void CrearUsuario(String userName, String password, String email) {
-        Usuario u = new Usuario();
-        u.setUsername(userName);
-        u.setPassword(password);
-        u.setEmail(email);
-        repo.save(u);
-
-    }
+    
 
     
 }
